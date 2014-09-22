8e954694e8b2be40f3bfde681d5f3336e46fde5c
Nacho Glinsek <iglinsek@gmail.com>
2014-09-22 20:37:07 -0300

    Added missing file

---
+++ FurryRun.Editor/Views/Stage/LayersWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace FurryRun.Editor.Views.Stage
+{
+    /// <summary>
+    /// Interaction logic for LayersWindow.xaml
+    /// </summary>
+    public partial class LayersWindow : Window
+    {
+        public LayersWindow()
+        {
+            InitializeComponent();
+            Closing += Window_Closing;
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close,
+                (sender, args) => Close()));
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        public void DragWindow(object sender, MouseButtonEventArgs args)
+        {
+            DragMove();
+        }
+    }
+}
